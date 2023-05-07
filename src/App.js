2c945dc3b53c6ac371476da2827f7aeb3013eca6
etikalarahul <mdkaifali17317@gmail.com>
2023-05-07 12:56:31 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';  
 import './App.css';
 import React, { useEffect } from "react"
 import Home from './Component/Home';
